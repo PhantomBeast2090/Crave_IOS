3946c2dc06c7c1f0a40f1914b00a62777f08021e
Rakshan_Adithyaa <rakshanadithyaa@gmail.com>
2026-09-05 10:25:32 +0530

    Initial Commit

---
+++ Crave_IOS/Crave_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crave_IOSApp.swift
+//  Crave_IOS
+//
+//  Created by Rakshan Adithyaa on 05/09/26.
+//
+
+import SwiftUI
+
+@main
+struct Crave_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
